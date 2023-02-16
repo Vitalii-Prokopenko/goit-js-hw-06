70f78e24a1786166c0405e601440e28d2f885948
Vitalii-Prokopenko <doc8259@gmail.com>
2023-02-16 20:41:01 +0200

    task-01 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const items = document.querySelectorAll(".item");
 console.log(`Number of categories: ${items.length}`);
-const categoryNames = document.querySelectorAll(".item h2");
 
+const categoryNames = document.querySelectorAll(".item h2");
 categoryNames.forEach((categoryName) => {
   console.log(`Category: ${categoryName.textContent}`);
   let categoryItems = categoryName.nextElementSibling.querySelectorAll("li");
