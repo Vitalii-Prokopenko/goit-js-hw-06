37bc84859f01df1fbec4a059734ff63f552602cd
Vitalii-Prokopenko <doc8259@gmail.com>
2023-02-19 17:24:53 +0200

    task #9 added

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+const handleClick = (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = body.style.backgroundColor;
+};
+
+changeColorBtn.addEventListener('click', handleClick);
+
