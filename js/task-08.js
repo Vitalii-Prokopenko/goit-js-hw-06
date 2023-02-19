29f101c1981eaf86d7cbccdc9dd34b027f89066d
Vitalii-Prokopenko <doc8259@gmail.com>
2023-02-19 17:08:59 +0200

    task #8 added

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const form = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+    
+    // Checks if all form fields are typed
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be typed");
+    };
+
+    // Creates an object with inputed field values
+
+  const user = {
+    email: email.value,
+    password: password.value,
+  };
+
+    console.log(user);
+    
+    // Resets form fields
+
+  form.reset();
+};
+
+form.addEventListener("submit", handleSubmit);
