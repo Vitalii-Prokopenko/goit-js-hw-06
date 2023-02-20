653c683504e8fe9553284cddcd75e33658749fd0
Vitalii-Prokopenko <doc8259@gmail.com>
2023-02-20 18:41:49 +0200

    task #5 fixed

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,10 @@
-const textInput = document.querySelector('#name-input');
-const textOutput = document.querySelector('#name-output');
+const textInput = document.querySelector("#name-input");
+const textOutput = document.querySelector("#name-output");
 
-textInput.addEventListener('input', (event) => {
+textInput.addEventListener("input", (event) => {
+  if (event.currentTarget.value === "") {
+    textOutput.textContent = "Anonymous";
+  } else {
     textOutput.textContent = event.currentTarget.value;
+    };
 });
